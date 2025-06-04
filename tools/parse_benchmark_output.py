7c6b385d0958ecc5a91755346f0483634659d881
Aditya Kashi <kashia@ornl.gov>
2025-06-04 10:46:16 -0400

    plotting memory BW outcomes

--- tools/parse_benchmark_output.py
+++ tools/parse_benchmark_output.py
@@ -2,58 +2,94 @@ import argparse
 import csv
 import os
 
-def parse_iterations_summary(file_path):
-    # Initialize a dictionary to store the extracted values
+def get_membw_line(results, line, part):
+    """
+    @param results  Output dict to add keys to.
+    @param line  Inputs file line to parse.
+    @param part  Prefix string for output key.
+    """
+    if "Raw Total" in line:
+        results[part+'_raw'] = float(line.split('=')[-1].strip())
+    elif "Raw Ortho" in line:
+        results[part+'_ortho'] = float(line.split('=')[-1].strip())
+    elif "Raw SpMV" in line:
+        results[part+'_spmv'] = float(line.split('=')[-1].strip())
+    elif "Raw MG" in line:
+        results[part+'_mg'] = float(line.split('=')[-1].strip())
+    elif "GS" in line:
+        results[part+'_mg_gs'] = float(line.split('=')[-1].strip())
+    elif "RestrPro" in line:
+        results[part+'_mg_rp'] = float(line.split('=')[-1].strip())
+    elif "Raw vecupd" in line:
+        results[part+'_vecupd'] = float(line.split('=')[-1].strip())
+
+def parse_iterations_summary(file_path, extr_type):
+    """ Parse the HPG-MxP output file for the needed metrics.
+
+    @param file_path  Path to HPG-MxP output file.
+    @param extr_type  Type of metrics to extract: "flops" or "membw"
+    """
     results = {}
     results["code"] = "present"
 
     # Open the file and read line by line
     with open(file_path, 'r') as file:
         for line in file:
-            # Check for desired keys and extract their values
             if "Reference version of ComputeSPMV" in line:
                 results["code"] = "baseline"
-            if "Machine Summary::Distributed Processes" in line:
+            elif "Machine Summary::Distributed Processes" in line:
                 results["n_procs"] = int(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary::Ortho" in line:
-                results["mxp_ortho_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary::SpMV" in line:
-                results["mxp_spmv_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary::MG" in line:
-                results["mxp_mg_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: SpTRSV" in line:
-                results["mxp_gs_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: Restic" in line:
-                results["mxp_restrict_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: - Ortho   (reference)" in line:
-                results["double_ortho_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: - SpMV    (reference)" in line:
-                results["double_spmv_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: - MG      (reference)" in line:
-                results["double_mg_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: -  SpTRSV (reference)" in line:
-                results["double_gs_time"] = float(line.split('=')[-1].strip())
-            elif "Benchmark Time Summary:: -  Restic (reference)" in line:
-                results["double_restrict_time"] = float(line.split('=')[-1].strip())
 
-            elif "GFLOP/s Summary::Total for benchmark" in line:
-                results['mxp_total_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary:: - Total     (reference)" in line:
-                results['double_total_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary::Raw Total" in line:
-                results['mxp_raw_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary::Raw Orho" in line:
-                results['mxp_ortho_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary::Raw SpMV" in line:
-                results['mxp_spmv_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary::Raw MG" in line:
-                results['mxp_mg_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary:: - Raw Orho  (reference)" in line:
-                results['double_ortho_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary:: - Raw SpMV  (reference)" in line:
-                results['double_spmv_gflops'] = float(line.split('=')[-1].strip())
-            elif "GFLOP/s Summary:: - Raw MG    (reference)" in line:
-                results['double_mg_gflops'] = float(line.split('=')[-1].strip())
+            if extr_type == "flops":
+                if "Benchmark Time Summary::Ortho" in line:
+                    results["mxp_ortho_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary::SpMV" in line:
+                    results["mxp_spmv_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary::MG" in line:
+                    results["mxp_mg_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: SpTRSV" in line:
+                    results["mxp_gs_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: Restic" in line:
+                    results["mxp_restrict_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: - Ortho   (reference)" in line:
+                    results["double_ortho_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: - SpMV    (reference)" in line:
+                    results["double_spmv_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: - MG      (reference)" in line:
+                    results["double_mg_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: -  SpTRSV (reference)" in line:
+                    results["double_gs_time"] = float(line.split('=')[-1].strip())
+                elif "Benchmark Time Summary:: -  Restic (reference)" in line:
+                    results["double_restrict_time"] = float(line.split('=')[-1].strip())
+
+                elif "GFLOP/s Summary::Total for benchmark" in line:
+                    results['mxp_total_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary:: - Total     (reference)" in line:
+                    results['double_total_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary::Raw Total" in line:
+                    results['mxp_raw_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary::Raw Orho" in line:
+                    results['mxp_ortho_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary::Raw SpMV" in line:
+                    results['mxp_spmv_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary::Raw MG" in line:
+                    results['mxp_mg_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary:: - Raw Orho  (reference)" in line:
+                    results['double_ortho_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary:: - Raw SpMV  (reference)" in line:
+                    results['double_spmv_gflops'] = float(line.split('=')[-1].strip())
+                elif "GFLOP/s Summary:: - Raw MG    (reference)" in line:
+                    results['double_mg_gflops'] = float(line.split('=')[-1].strip())
+            elif extr_type == "membw":
+                if "Memory Bandwidth (GB/s)" in line:
+                    if "mxp" in line:
+                        get_membw_line(results, line, "mxp")
+                    elif "Total for benchmark" in line:
+                        results['mxp_total'] = float(line.split('=')[-1].strip())
+                    elif "reference" in line:
+                        get_membw_line(results, line, "double")
+            else:
+                raise "Invalid extraction type!"
 
     return results
 
@@ -77,11 +113,12 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse iteration summary from a text file.')
     parser.add_argument('file_path', type=str, help='Path to the input text file')
     parser.add_argument('output_file', type=str, help='Path to the output CSV file')
+    parser.add_argument('-e', '--extraction_type', type=str, help='Type of data to extract - "flops", "membw"')
     
     # Parse the command line arguments
     args = parser.parse_args()
 
-    results = parse_iterations_summary(args.file_path)
+    results = parse_iterations_summary(args.file_path, args.extraction_type)
     
     # Save the results to a CSV file
     save_to_csv(results, args.output_file)
--- tools/plot_scaling.py
+++ tools/plot_scaling.py
@@ -3,16 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
 
-markers = ['o', 's', '^', '>', '<']
-lines = ['-', ':', '-.', '--']
+markers = ['o', 's', '^', '>', '<']*2
+lines = ['-', ':', '-.', '--', '--']*2
 plt.rcParams.update({'font.size': 16})
 
-def plot_timings_over_n_procs(csv_file, npn, metrics, output_pref):
+def plot_timings_over_n_procs(csv_file, npn, metrics, components, output_pref):
     # Read the CSV file
     df = pd.read_csv(csv_file)
 
     df_cur = df[df["code"] == "present"].reset_index(drop=True)
     df_old = df[df["code"] == "sota"].reset_index(drop=True)
+    if "flop" in metrics[0]:
+        y_label = "GFLOPS/GCD"
+    else:
+        y_label = "GB/s/GCD"
 
     # Set up the plot
     plt.figure(figsize=(10, 6))
@@ -22,14 +26,14 @@ def plot_timings_over_n_procs(csv_file, npn, metrics, output_pref):
         if metric not in df.columns:
             print(f"Warning: Metric '{metric}' not found in the DataFrame.")
             return
-        metlabel = metric.split("_")[0]
+        metlabel = metric if components else metric.split("_")[0]
         if not df_cur.empty:
             plt.semilogx(df_cur['n_procs']/npn, df_cur[metric]/df_cur["n_procs"], marker=markers[i],
                          ls=lines[i], label=df_cur.at[0,"code"] + " " + metlabel)
         if not df_old.empty:
             plt.semilogx(df_old['n_procs']/npn, df_old[metric]/df_old["n_procs"], marker=markers[i],
                          ls=lines[i], label=df_old.at[0,"code"] + " " + metlabel)
-    plt.ylabel('GFLOPS/GCD')
+    plt.ylabel(y_label)
 
     # Add labels and title
     plt.legend(loc="upper center", bbox_to_anchor=(0.5, -0.1), ncol=4)
@@ -39,6 +43,10 @@ def plot_timings_over_n_procs(csv_file, npn, metrics, output_pref):
     x_ticks = np.unique((df['n_procs']/npn).astype('int32'))  # Use unique values from the DataFrame
     tixlabels = [val if val != 8192 else '' for val in x_ticks]
     plt.xticks(x_ticks, labels=tixlabels)
+
+    # For memBW plots, adjust y lims
+    if "GB/s" in y_label:
+        plt.ylim(bottom=400)
 
     # Save the plot to a file
     plt.savefig(output_pref + ".png", dpi=300, bbox_inches="tight")
@@ -53,17 +61,31 @@ if __name__ == "__main__":
     parser.add_argument('input_csv', type=str, help='Path to the input CSV file')
     parser.add_argument('output_prefix', type=str, help='Path to the output graph file (image)')
     parser.add_argument('--nprocs_per_node', type=int, default=8, help='Number of GPUs per node')
+    parser.add_argument('-m','--metric_type', type=str, default='flops',
+            help='Type of metric to plot the scaling of - "flops" or "membw"')
+    parser.add_argument('-c','--components', action='store_true', default=False,
+            help='Option to enable plot of all components')
 
     # Parse the command line arguments
     args = parser.parse_args()
 
     # Specify the timing metrics you want to plot
-    metrics_to_plot = [
-        'mxp_total_gflops',
-        'double_total_gflops',
-        # Add more metrics as needed
-    ]
+    metrics_to_plot = []
+    if args.metric_type == "flops":
+        metrics_to_plot = [
+            'mxp_total_gflops',
+            'double_total_gflops',
+            # Add more metrics as needed
+        ]
+    elif args.metric_type == "membw":
+        metrics_to_plot = ['mxp_total', 'double_raw']
+        if args.components:
+            metrics_to_plot = ['mxp_ortho', 'mxp_spmv', 'mxp_mg_gs', 'mxp_mg_rp', 'mxp_vecupd',
+                               'double_ortho', 'double_spmv', 'double_mg_gs', 'double_mg_rp',
+                               'double_vecupd']
+    else:
+        raise f"Invalid metric type {args.metric_type}!"
 
     # Call the plotting function
     plot_timings_over_n_procs(args.input_csv, args.nprocs_per_node, metrics_to_plot,
-                              args.output_prefix)
+                              args.components, args.output_prefix)
