ea1c3b5d8888bea9e6bb8213c61cb745ba87c66d
nkoukpaizan <nkoukpaizan@users.noreply.github.com>
2026-04-06 16:32:26 +0000

    Apply pre-commmit fixes

--- src/estimate_run_time.cpp
+++ src/estimate_run_time.cpp
@@ -7,7 +7,7 @@
 
 template<typename scalar_type, typename scalar_type2, class GMRESData_type, class GMRESData_type2>
 double estimate_run_time(comm_type comm,
-                         const SparseMatrix<scalar_type, scalar_type>& A, 
+                         const SparseMatrix<scalar_type, scalar_type>& A,
 #ifdef HPGMP_WITH_GINKGO_AMP
                          const SparseMatrix<scalar_type, scalar_type2>& A_lo,
 #else
